24d9cbb64e5e3b666346f1b0d3b85be4b05d9614
takaking216 <takaking216@yahoo.co.jp>
2021-01-02 03:45:41 +0900

    Initial commit

---
+++ app/src/main/java/com/example/buttonmashing/TitleFragment.kt
@@ -0,0 +1,38 @@
+package com.example.buttonmashing
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.util.Log
+import com.example.buttonmashing.databinding.FragmentTitleBinding
+
+
+class TitleFragment : Fragment() {
+
+    private var _binding: FragmentTitleBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTitleBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.startButton.setOnClickListener {
+            Log.d("click", "Clickされた")
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
