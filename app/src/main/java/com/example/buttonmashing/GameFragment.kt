5e26ea48e714217de11c787e774655f9dc298fc3
takaking216 <takaking216@yahoo.co.jp>
2021-01-03 03:11:13 +0900

    GameFragmentをdatabindingして連打回数表示できるようにした。

--- app/src/main/java/com/example/buttonmashing/GameFragment.kt
+++ app/src/main/java/com/example/buttonmashing/GameFragment.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-
+import androidx.databinding.DataBindingUtil
+import com.example.buttonmashing.databinding.FragmentGameBinding
+import java.lang.RuntimeException
 
 class GameFragment : Fragment() {
+    private val gameViewModel = GameViewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +18,16 @@ class GameFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding: FragmentGameBinding =
+                DataBindingUtil.bind(view)
+                        ?: throw
+        RuntimeException("FragmentGameBindingでバインディングができない")
+        binding.viewModel = gameViewModel
+        binding.lifecycleOwner = viewLifecycleOwner
     }
 }
