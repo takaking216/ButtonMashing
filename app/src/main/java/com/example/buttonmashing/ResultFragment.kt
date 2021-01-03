c14ed50b8a102d0d6d8695cdae06851cf83d7a26
takaking216 <takaking216@yahoo.co.jp>
2021-01-03 14:49:04 +0900

    リザルト画面に結果を表示するようにした。

--- app/src/main/java/com/example/buttonmashing/ResultFragment.kt
+++ app/src/main/java/com/example/buttonmashing/ResultFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.buttonmashing.databinding.FragmentResultBinding
+import java.lang.RuntimeException
 
 class ResultFragment : Fragment() {
+    val viewModel = ResultViewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,5 +18,20 @@ class ResultFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_result, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding: FragmentResultBinding =
+            DataBindingUtil.bind(view) ?: throw
+                    RuntimeException("FragmentResultBindingでバインドできない")
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val count = arguments?.getInt("count", 0) ?: 0
+        viewModel.count.value = count
     }
 }
---
+++ app/src/main/java/com/example/buttonmashing/ResultViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.buttonmashing
+
+import androidx.lifecycle.MutableLiveData
+
+class ResultViewModel {
+    val count = MutableLiveData<Int>(0)
+}
